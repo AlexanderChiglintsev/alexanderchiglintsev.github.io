caeda0cdbd9422341246d94cf4001a2ceaa01a5a
Alexander Chiglintsev <alex.chiglintsev@yandex.ru>
2021-05-17 17:00:25 +0900

    Add web files and resources

---
+++ js/new_myriad.js
@@ -0,0 +1,20 @@
+/*!
+ * The following copyright notice may not be removed under any circumstances.
+ * 
+ * Copyright:
+ * Copyright © 2000 Adobe Systems Incorporated. All Rights Reserved. U.S. Patent
+ * Des. pending.
+ * 
+ * Trademark:
+ * Myriad is a registered trademark of Adobe Systems Incorporated.
+ * 
+ * Designer:
+ * Robert Slimbach and Carol Twombly
+ * 
+ * Vendor URL:
+ * http://www.adobe.com/type
+ * 
+ * License information:
+ * http://www.adobe.com/type/legal.html
+ */
+Cufon.registerFont({"w":0,"face":{"font-family":"","font-weight":"","font-stretch":"","font-style":"normal","units-per-em":"","panose-1":"","ascent":"","descent":"","bbox":"","underline-thickness":"","underline-position":"","unicode-range":""},"glyphs":{" ":{},"\u00a0":{}}});
